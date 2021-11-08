d65010a8329008972abf33dab57f74cb1470db80
jörgen persson <jorgen_raby@icloud.com>
2021-11-08 14:42:29 +0100

    Initial Commit

---
+++ nyGitHubTest/nyGitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  nyGitHubTestApp.swift
+//  nyGitHubTest
+//
+//  Created by jörgen persson on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct nyGitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
